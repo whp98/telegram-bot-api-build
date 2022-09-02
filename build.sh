37586520dec8a9de6f955cdd9ef5229779c89482
whp98 <whp98@outlook.com>
2022-09-02 17:27:17 +0800

    test1

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-echo '
-# clang
+sudo echo '
+deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy main
+deb-src http://apt.llvm.org/jammy/ llvm-toolchain-jammy main
+# 14
 deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-14 main
 deb-src http://apt.llvm.org/jammy/ llvm-toolchain-jammy-14 main
+# 15
+deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-15 main
+deb-src http://apt.llvm.org/jammy/ llvm-toolchain-jammy-15 main
 ' >> /etc/apt/sources.list
+cat /etc/apt/sources.list
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-get update
 sudo apt-get upgrade
+sudo apt-get install clang-14 lldb-14 lld-14
 sudo apt-get install make git zlib1g-dev libssl-dev gperf cmake clang-14 libc++-dev libc++abi-dev
 git clone --recursive https://github.com/tdlib/telegram-bot-api.git
 cd telegram-bot-api
