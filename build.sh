3b1b20dd7003e478b07a9e55c9f14a495c75cc45
whp98 <whp98@outlook.com>
2022-09-03 14:23:28 +0800

    test

--- build.sh
+++ build.sh
@@ -15,14 +15,14 @@ wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install clang-14 lldb-14 lld-14
-# sudo apt-get install make git zlib1g-dev libssl-dev gperf cmake clang-14 libc++-dev libc++abi-dev
-# git clone --recursive https://github.com/tdlib/telegram-bot-api.git
-# cd telegram-bot-api
-# rm -rf build
-# mkdir build
-# cd build
-# export CXXFLAGS="-stdlib=libc++"
-# CC=/usr/bin/clang-14 CXX=/usr/bin/clang++-14 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=.. ..
-# cmake --build . --target install
-# cd ../..
-# ls -l telegram-bot-api/bin/telegram-bot-api*
+sudo apt-get install make git zlib1g-dev libssl-dev gperf cmake clang-14 libc++-dev libc++abi-dev
+git clone --recursive https://github.com/tdlib/telegram-bot-api.git
+cd telegram-bot-api
+rm -rf build
+mkdir build
+cd build
+export CXXFLAGS="-stdlib=libc++"
+CC=/usr/bin/clang-14 CXX=/usr/bin/clang++-14 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=.. ..
+cmake --build . --target install
+cd ../..
+ls -l telegram-bot-api/bin/telegram-bot-api*
