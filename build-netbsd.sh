f5a76f55f01ec2799ebafdb6155990a9f42a9896
whp98 <whp98@outlook.com>
2022-09-04 01:15:36 +0800

    build

--- build-netbsd.sh
+++ build-netbsd.sh
@@ -1,6 +1,6 @@
 su -
 export PKG_PATH=https://ftp.netbsd.org//pub/pkgsrc/packages/NetBSD/x86_64/9.2/All/
-pkg_add git gperf cmake openssl gcc5-libs
+pkg_add git gperf cmake openssl gcc12-libs
 exit
 git clone --recursive https://github.com/tdlib/telegram-bot-api.git
 cd telegram-bot-api
