97f3efe77b3757ea479c2b523ce320e7d674b9a4
whp98 <whp98@outlook.com>
2022-09-03 19:25:50 +0800

    test

---
+++ build-macos-intel.sh
@@ -0,0 +1,12 @@
+xcode-select --install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install gperf cmake openssl
+git clone --recursive https://github.com/tdlib/telegram-bot-api.git
+cd telegram-bot-api
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl/ -DCMAKE_INSTALL_PREFIX:PATH=.. ..
+cmake --build . --target install
+cd ../..
+ls -l telegram-bot-api/bin/telegram-bot-api*
