c0e18af7b01d1a8f18aa1c582f5ac6706c67d4d1
whp98 <whp98@outlook.com>
2022-09-03 14:45:04 +0800

    sss

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ deb http://archive.canonical.com/ubuntu/ jammy partner
 cat /etc/apt/sources.list
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-get update
-sudo apt-get upgrade
+# sudo apt-get upgrade
 sudo apt-get install clang-14 lldb-14 lld-14
 sudo apt-get install make git zlib1g-dev libssl-dev gperf cmake clang-14 libc++-dev libc++abi-dev
 git clone --recursive https://github.com/tdlib/telegram-bot-api.git
